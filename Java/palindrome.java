c0d684abc7cdc67f47cdcd88e52cc2e213a69a38
Chirag <chiragrrai44@gmail.com>
2026-02-10 14:20:39 +0530

    code to check palindrome

---
+++ Java/palindrome.java
@@ -0,0 +1,15 @@
+class Solution {
+    boolean isPalindrome(String s) {
+    int r=s.length()-1,l=0;
+    while(l<r)
+    {
+        if(s.charAt(l)!=s.charAt(r))
+        {
+            return false;
+        }
+        l++;
+        r--;
+    }
+    return true;
+    }
+}
