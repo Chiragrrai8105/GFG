db5b9d13c29a84304c9305d5aabef651419d12b3
Chirag <chiragrrai44@gmail.com>
2026-02-26 16:32:24 +0530

    find position of the first repeated position

---
+++ Java/findpos.java
@@ -0,0 +1,15 @@
+class findpos {
+    public static int firstRepeated(int[] arr) {
+        for(int i=0;i<arr.length;i++)
+        {
+            for(int j=i+1;j<arr.length;j++)
+            {
+                if(arr[i]==arr[j])
+                {
+                    return i+1;
+                }
+            }
+        }
+        return -1;
+    }
+}
