869367f9298b6c63b19da06eb97c4e6628c4ea09
Chirag <chiragrrai44@gmail.com>
2026-03-06 21:50:25 +0530

    check the string is palindrome using two pointers

---
+++ Java/isPossiblePalindrome.java
@@ -0,0 +1,20 @@
+class isPossiblePalindrome {
+
+    public int demo(String s, int K) {
+        String n=s;
+        for(int j=0;j<K;j++)
+        {
+            n+=s.charAt(j);
+        }
+        int a=n.length()-1;
+        for(int b=0;b<n.length();b++)
+        {
+            if(n.charAt(b)!=n.charAt(a))
+            {
+                return 0;
+            }
+            a--;
+        }
+        return 1;
+    }
+}
