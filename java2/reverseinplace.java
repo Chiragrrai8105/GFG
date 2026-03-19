7dace29f1604c845432fce4b31b7be73cabd104d
Chirag <chiragrrai44@gmail.com>
2026-03-19 19:03:38 +0530

    reverse an array in place

--- java2/reverseinplace.java
+++ java2/reverseinplace.java
@@ -1,12 +1,18 @@
 class reverseinplace {
-    public void reverseArray(int arr[]) {
+    public static void main(String[] args){
+    int arr[]={1,2,3,4};
     int l=0,r=arr.length-1;
     while(l<r)
     {
        int temp=arr[l];
         arr[l]=arr[r];
         arr[r]=temp;
+        l++;
+        r--;
     }
-    return;
+    for(int i=0;i<arr.length;i++)
+    {
+        System.out.println(arr[i]);
+    }
     }
 }
