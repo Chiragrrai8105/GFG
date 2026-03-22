09b76c7e09e07b96b0858b017626320136a28e02
Chirag <chiragrrai44@gmail.com>
2026-03-22 20:07:48 +0530

    LCM and GCD

---
+++ java2/lcmgcd.java
@@ -0,0 +1,18 @@
+class lcmgcd {
+    public static int[] lcmAndGcd(int a, int b) {
+        // code here
+        int x=a,y=b;
+        while (y!=0)
+        {
+            int temp=y;
+            y=x%y;
+            x=temp;
+        
+        }
+        int gcd=x;
+        int lcm=(a/gcd)*b;
+        
+        return new int[]{lcm,gcd};
+    }
+    
+}
