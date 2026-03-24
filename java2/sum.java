71b15249aeafa306c9ed904bd1af17fbece2e03a
Chirag <chiragrrai44@gmail.com>
2026-03-24 18:57:01 +0530

    sum expect first and last elements

---
+++ java2/sum.java
@@ -0,0 +1,10 @@
+class sum {
+    int sumExceptFirstLast(int[] arr) {
+        int sum=0;
+        for(int i=1;i<arr.length-1;i++)
+        {
+            sum+=arr[i];
+        }
+        return sum;
+    }
+}
